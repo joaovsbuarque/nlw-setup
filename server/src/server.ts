a10b7ccb662672877538c7d4e45676b177747e8f
joaovsbuarque <joaobuarqueestudos@gmail.com>
2023-01-19 19:36:06 -0300

    feat: update server prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,11 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
+import { appRouter } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/hello", async () => {
-  const habits = await prisma.habit.findMany({});
-  return habits;
-});
+app.register(appRouter);
 
 app
   .listen({
