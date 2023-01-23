d82202027c43b441cd8737620fe161733af6c156
joaovsbuarque <joaobuarqueestudos@gmail.com>
2023-01-23 16:21:11 -0300

    feat: update components

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,42 +1,78 @@
-import { genateteDatesFromYearBeginning } from "../utils/genarete-dates-from-years-beginning";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay";
 
-const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const weekDays = [
+  'D',
+  'S',
+  'T',
+  'Q',
+  'Q',
+  'S',
+  'S',
+];
 
-const summaryDates = genateteDatesFromYearBeginning();
+const summaryDates = generateDatesFromYearBeginning()
 
-const minimumSumaryDatesSize = 18 * 7;
-const amoutOfDatesToFill = minimumSumaryDatesSize - summaryDates.length;
+const minimumSummaryDatesSize = 18 * 7 // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
+type Summary = {
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}[]
 
 export function SummaryTable() {
+
+  const [summary, setSummary] = useState<Summary>([])
+
+  useEffect(() =>{
+    api.get('summary').then(response => {
+      setSummary(response.data)
+    })
+  },[])
+
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weekDays.map((weekDays, i) => {
+        {weekDays.map((weekDay, i) => {
           return (
             <div
-              key={`${weekDays}-${i}`}
-              className="text-zinc-400 text-xl h-10 w-10 item-center justify-center font-bold"
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
             >
-              {weekDays}
+              {weekDay}
             </div>
-          );
+          )
         })}
       </div>
+
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((date) => {
-          return <HabitDay amount={5} completed={3} key={date.toString()} />;
+        {summary.length &&summaryDates.map((date) => {
+
+          const dayInSummary = summary.find(day => {
+            return dayjs(date).isSame(day.date, 'day')
+          })
+
+          return (
+            <HabitDay 
+              key={date.toString()}
+              date={date}
+              amount={dayInSummary?.amount} 
+              defaultCompleted={dayInSummary?.completed} 
+            />
+          )
         })}
 
-        {amoutOfDatesToFill > 0 &&
-          Array.from({ length: amoutOfDatesToFill }).map((_, i) => {
-            return (
-              <div
-                key={i}
-                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
-              />
-            );
-          })}
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+          return (
+            <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+          )
+        })}
       </div>
     </div>
   );
